dc417f8f5c90dfa89feb7e4505a305b0751b1e6c
mfclinton <mfclinton0@gmail.com>
2023-02-21 19:08:03 -0500

    clean up unused ML code

--- UnityProject/Assets/Scripts/AICore/LargeLanguageModels/GPT2Inference.cs
+++ UnityProject/Assets/Scripts/AICore/LargeLanguageModels/GPT2Inference.cs
@@ -67,90 +67,9 @@ namespace AICore
         }
 
         /// <summary>
-        /// Modifies the `orderedZipped` list in place, leaving only the topP percent of tokens
-        /// </summary>
-        static void TopP(List<(float, int)> orderedZipped, float topP)
-        {
-            if (topP == 1f)
-                return;
-
-            List<(float, int)> probs = MathUtils.Probabilities.CalculateProbs(orderedZipped);
-
-            float cummProb = 0f;
-            int index = 0;
-            foreach ((float prob, int idx) in probs)
-            {
-                cummProb += prob;
-                index++;
-
-                if (topP <= cummProb)
-                    break;
-            }
-
-            if (index != orderedZipped.Count())
-            {
-                orderedZipped.RemoveRange(index, orderedZipped.Count() - index);
-            }
-        }
-
-        /// <summary>
         /// Constructs the next n most likely tokens in a sequence given an input string
         /// </summary>
-        public static string CausalLMDefaultGeneration(InferenceSession session, Tokenizer tokenizer, string input, int n)
-        {
-            List<long> encodedInputSeq = tokenizer.Encode(input).ToList();
-
-            for (int i = 0; i < n; i++)
-            {
-                float[] logits = CausalLMPrediction(session, encodedInputSeq.ToArray());
-                List<(float, int)> orderedZipped = ProcessLogits(logits);
-
-                encodedInputSeq.Add(orderedZipped[0].Item2);
-            }
-
-            string result = tokenizer.Decode(encodedInputSeq.ToArray());
-            return result;
-        }
-
-        /// <summary>
-        /// Constructs the next word / finishes the last word in a sequence
-        /// </summary>
-        public static string NextWordPrediction(InferenceSession session, Tokenizer tokenizer, string input)
-        {
-            List<long> encodedInputSeq = tokenizer.Encode(input).ToList();
-            string newWord = "";
-
-            int MAX_CHUNKS = 5;
-            for (int i = 0; i < MAX_CHUNKS; i++)
-            {
-                float[] logits = CausalLMPrediction(session, encodedInputSeq.ToArray());
-                List<(float, int)> orderedZipped = ProcessLogits(logits);
-
-                long token = orderedZipped[0].Item2;                
-                string chunk = tokenizer.Decode(new long[] { token });
-
-                if (newWord.Length != 0 && (chunk.StartsWith(" ") || chunk.StartsWith(".")))
-                {
-                    Debug.Log("Exited Before Updating Word");
-                    break;
-                }
-
-                encodedInputSeq.Add(token);
-                newWord += chunk;
-                if (newWord.EndsWith(" ") || newWord.EndsWith("."))
-                {
-                    Debug.Log("Exited After Updating Word");
-                    break;
-                }
-            }
-
-            return newWord;
-        }
-
-        /// <summary>
-        /// Constructs the next n most likely tokens in a sequence given an input string
-        /// </summary>
-        public static List<(float, string)> RecommendedNextWords(InferenceSession session, Tokenizer tokenizer, string input, int branches, float sensitivity = 0.1f, int maxWordCount = 20)
+        public static List<(float, string)> RecommendedNextWords(InferenceSession session, Tokenizer tokenizer, string input, int branches, float sensitivity = 0.1f, int maxWordCount = 8)
         {
             List<(float, string)> completedWords = new List<(float, string)>();
             Queue<(float, string)> queuedWords = new Queue<(float, string)>();
