e6bf7f55660fd8699e5544ae31c77898479693e8
mfclinton <mfclinton0@gmail.com>
2023-02-24 22:11:59 -0500

    add animation and scale mouse

--- UnityProject/Assets/Scripts/Managers/MouseManager.cs
+++ UnityProject/Assets/Scripts/Managers/MouseManager.cs
@@ -3,40 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Animator))]
 public class MouseManager : MonoBehaviour
 {
-    [SerializeField] Texture2D crosshair;
-    [SerializeField] Vector2 offsetFactor = Vector2.one;
-    [SerializeField] float radiusFactor = 1f;
-    [SerializeField] Image debugCircle;
+    [SerializeField] GameObject mousePrefab;
+    [SerializeField] float radiusScalar;
+
+    RectTransform mouse;
+    Animator animator;
     KeyboardManager keyboardManager;
 
     private void Awake()
     {
+        // Cursor.visible = false;
+        
+        Canvas canvas = FindObjectOfType<Canvas>();
+        mouse = Instantiate(mousePrefab, canvas.transform).GetComponent<RectTransform>();
+        animator = mouse.GetComponent<Animator>();
+
         keyboardManager = FindObjectOfType<KeyboardManager>();
-        SetCursor(crosshair, offsetFactor, radiusFactor);
     }
 
-    public void Update()
+    private void Update()
     {
-        // DebugCursor();
+        Canvas canvas = FindObjectOfType<Canvas>();
+
+        // Radius
+        KeyboardManager.radius = mouse.rect.width * canvas.scaleFactor * radiusScalar / 2f;
+        mouse.position = Input.mousePosition;
+
         if (Input.GetMouseButtonDown(0))
+        {
             keyboardManager.HandleClick();
-    }
-
-    private void DebugCursor()
-    {
-        // TODO: Fix circle scale
-        debugCircle.transform.GetComponent<RectTransform>().sizeDelta = Vector2.one * (crosshair.width * radiusFactor);
-        debugCircle.transform.position = Input.mousePosition;
-    }
-
-    private void SetCursor(Texture2D texture, Vector2 offsetFactor, float radiusFactor)
-    {
-        //set the cursor origin to its centre. (default is upper left corner)
-        Vector2 cursorOffset = new Vector2(texture.width / 2, texture.height / 2) * offsetFactor;
-        Cursor.SetCursor(texture, cursorOffset, CursorMode.ForceSoftware);
-        KeyboardManager.radius = texture.width * radiusFactor;
-
+            animator.SetTrigger("click");
+        }
     }
 }
